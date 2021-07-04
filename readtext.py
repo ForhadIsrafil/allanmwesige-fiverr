8818bdabcff9a413405888aa8fe904cf87ea5f5d
forhadisrafil <forhadtuhin5088@gmail.com>
2021-07-04 22:55:56 +0600

    up

--- readtext.py
+++ readtext.py
@@ -6,4 +6,4 @@ import pandas as pd
 
 df = pd.read_csv('Influencer names and ID.txt', sep='\t')
 print(df[df['Username'] == 'makeupbynvs']['#Followers'].values[0])
-# print(df.shape)
+print(df.count())
