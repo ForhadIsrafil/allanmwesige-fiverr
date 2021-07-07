2e2c15e6a8a938af521975fde6f7e8396ce3e966
forhadisrafil <forhadtuhin5088@gmail.com>
2021-07-07 22:12:00 +0600

    init

--- readtext.py
+++ readtext.py
@@ -4,6 +4,12 @@ import datetime
 
 import pandas as pd
 
-df = pd.read_csv('Influencer names and ID.txt', sep='\t')
-print(df[df['Username'] == 'makeupbynvs']['#Followers'].values[0])
-print(df.count())
+# df = pd.read_csv('Influencer names and ID.txt', sep='\t')
+# print(df[df['Username'] == 'makeupbynvs']['#Followers'].values[0])
+# print(df.count())
+
+
+df1 = pd.read_csv('json_data_1.csv')
+df2 = pd.read_csv('json_data_2.csv')
+df3 = pd.concat([df1, df2],)
+df3.to_csv('final.csv', index=False)
